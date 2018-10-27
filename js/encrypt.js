5030e76f2cb06b39b287b6b7ecbbd818449c909b
Chaobai Li <contact@chaobai.li>
2018-10-27 13:31:17 +0200

    crypto function works

--- js/encrypt.js
+++ js/encrypt.js
@@ -1,7 +1,47 @@
 $(function(){
 //----------------------------------------------------------------------------
 
+function encodeUTF8(arr) {
+    var i, s = [];
+    for (i = 0; i < arr.length; i++) s.push(String.fromCharCode(arr[i]));
+    return decodeURIComponent(escape(s.join('')));
+};
+
+
+var myKeys = openpgp.key.readArmored($("#keyring").text()).keys;
+
+$("#encrypt button").click(function(){
+    var self = this;
+    var role = $(this).attr("data-role");
+    if(role == "encrypt"){
+        var plaintext = $("#encrypt textarea").val();
+        var ciphertext = openpgp.encrypt({
+            data: plaintext,
+            publicKeys: myKeys,
+        }).then(function(ciphertext){
+            $("#encrypt textarea").val(ciphertext.data);
+            $(self)
+                .attr("data-role", "clear")
+                .text($(self).attr("data-clear-text"))
+            ;
+        });
+    } else {
+        if(!confirm("确定要清除已经加密的文本？ / Are you sure to clear the encrypted message?")) return;
+        $("#encrypt textarea").val("");
+        $(self)
+            .attr("data-role", "encrypt")
+            .text($(self).attr("data-encrypt-text"))
+        ;
+    }
+});
+
+
+
 function showStatus(verified){
+    if(!verified){
+        $("#result").removeClass("error success");
+        return;
+    }
     var success = false;
     console.log(verified);
     try{
@@ -12,18 +52,31 @@ function showStatus(verified){
         .removeClass("error success")
         .addClass((success?"success":"error"))
     ;
-    $("#original").val(verified.data);
+    if(typeof verified.data == "string" || verified.data instanceof String){
+        $("#original").val(verified.data);
+    } else {
+        $("#original").val(encodeUTF8(verified.data));
+    }
 }
 
 function verify(){
-    var data = $("#data").val(),
-        keyring = $("#keyring").text();
+    var data = $("#data").val();
     try{
+        if(data.indexOf("PGP SIGNED MESSAGE") >= 0){
+            var message = openpgp.cleartext.readArmored(data);
+        } else {
+            var message = openpgp.message.readArmored(data);
+        }
         openpgp.verify({
-            message: openpgp.cleartext.readArmored(data),
-            publicKeys: openpgp.key.readArmored(keyring).keys,
-        }).then(showStatus);
+            message: message,
+            publicKeys: myKeys,
+        }).then(showStatus).catch(function(e){
+            alert("输入不正确，这不是一条被签署的消息。/ Incorrect input: not a message with signature.");
+            showStatus(false);
+        });
     }catch(e){
+        console.error("Error",e);
+        alert("输入不正确，这不是一条被签署的消息。/ Incorrect input: not a message with signature.");
         showStatus(false);
     }
     $("#data").val("");
